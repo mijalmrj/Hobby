1f118e5d9442ec4b3ee076112fa8eec1fce7f2d6
mijalmrj <mijalmrz@gmail.com>
2024-05-12 23:11:27 +1000

    change

--- my house.php
+++ my house.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Study Subjects</title>
+    <title>My House</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
@@ -22,21 +22,25 @@
         </nav>
     </header>
 
-    <main>
-        <section id="study-subject">
-            <h1>My Study Subject</h1>
-            <ol>
-                <li>Cloud and Beyond</li>
-                <li>Data Security</li>
-                <li>Developing Web Information Systems</li>
-                <li>Software Engineering</li>
-            </ol>
-            <p><a href="#">Schedule</a> <img src="images/schedule_image.png" alt="Schedule Image" title="Schedule Image"></p>
-        </section>
+    <main style="justify-content: center; display: flex; ">
+    <?php
+$houseInfo = array(
+    "Bedrooms" => 4,
+    "Bathrooms" => 2,
+    "Address" => "420 Ktm St"
+);
+
+echo "<table class='tg' border='1'>";
+echo "<tr><th class='tg-head'>Context</th><th class='tg-head'>Value</th></tr>";
+foreach ($houseInfo as $attribute => $value) {
+    echo "<tr><td class='tg-fuxe'>$attribute</td><td class='tg-fuxe'>$value</td></tr>";
+}
+echo "</table>";
+?>
     </main>
 
 
-    <footer>
+    <footer style="position: absolute;">
         <div class="contact-info">
             <p>Email: K210844@student.kent.edu.au</p>
             <p>Phone: +6123456789</p>
