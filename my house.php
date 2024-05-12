7036fc70dceb4f056649f83aa48592bfaa4ff89b
mijalmrj <mijalmrz@gmail.com>
2024-05-12 19:21:51 +1000

    change

---
+++ my house.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Study Subjects</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header class="header">
+        <img src="Images/Mijal Maharjan.png" alt="Logo" class="logo">
+        <nav class="horizontal-menu">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="my study subjects.html">My Study Subject</a></li>
+                <li><a href="my hobbies.html">My Hobbies</a></li>
+                <li><a href="my music.html">My Music</a></li>
+                <li><a href="my house.php">My House</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="study-subject">
+            <h1>My Study Subject</h1>
+            <ol>
+                <li>Cloud and Beyond</li>
+                <li>Data Security</li>
+                <li>Developing Web Information Systems</li>
+                <li>Software Engineering</li>
+            </ol>
+            <p><a href="#">Schedule</a> <img src="images/schedule_image.png" alt="Schedule Image" title="Schedule Image"></p>
+        </section>
+    </main>
+
+
+    <footer>
+        <div class="contact-info">
+            <p>Email: K210844@student.kent.edu.au</p>
+            <p>Phone: +6123456789</p>
+            <p>Social: <a href="https://steamcommunity.com/profiles/76561198415213032/">MIZ</a></p>
+        </div>
+        <p>&copy; 2024 Mijal Maharjan</p>
+    </footer>
+</body>
+
+</html>
